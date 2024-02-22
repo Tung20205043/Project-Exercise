16a0eb4897d1467e5b59f589ff9cb434862ee42d
Tung20205043 <letung10112002@gmail.com>
2024-02-22 20:26:59 +0700

    no message

--- Assets/Exercise1/RotateQuad.cs
+++ Assets/Exercise1/RotateQuad.cs
@@ -1,27 +1,24 @@
 ﻿using UnityEngine;
 
 public class RotateQuad : MonoBehaviour {
-    #region Singleton
-    public static RotateQuad Instance { get; private set; }
+    
 
-    private void Awake() {
-        if (Instance != null) {
-            Destroy(gameObject);
-            return;
-        }
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
-    }
-    #endregion
     private readonly float rotationSpeed = 3f;
     private Vector3 lastMousePosition;
 
     void Update() {
         if (Input.GetMouseButton(0)) {
-            Vector3 deltaMousePosition = Input.mousePosition - lastMousePosition;
-            float angle = deltaMousePosition.x * rotationSpeed;
-            transform.Rotate(0f, 0f, -angle);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask.GetMask("Quad"))) {
+                Vector3 targetDirection = hit.point - transform.position;
+                float angle = Vector3.Angle(transform.forward, targetDirection);
+                Vector3 cross = Vector3.Cross(transform.forward, targetDirection);
+                if (cross.y < 0) {
+                    angle = -angle;
+                }
+                transform.Rotate(0f, 0f, -angle * rotationSpeed * Time.deltaTime);
+            }
         }
-        lastMousePosition = Input.mousePosition;
     }
 }
